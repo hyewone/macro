bfccaa55ab65fe32c38b7ad3886ca23062cb63d7
hyewone <won5854@naver.com>
2022-06-23 06:37:13 +0900

    no message

--- macro.py
+++ macro.py
@@ -8,11 +8,16 @@ from PyQt5.QtCore import QCoreApplication
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
 
-# 문자인식
 import easyocr
+import time
+import traceback
 
 driver = webdriver.Chrome('C:\chromedriver.exe')
+driver.set_window_size(800, 1000)  # (가로, 세로)
+
+step = 0
 
 class MyApp(QWidget):
 
@@ -23,9 +28,21 @@ class MyApp(QWidget):
     def initUI(self):
         # 기본설정
         self.setWindowTitle('My First Application')
-        self.move(300, 300)
+        self.move(1400, 300)
         self.resize(400, 200)
         # self.setGeometry(300, 300, 300, 200) : move + resize
+
+        #  버튼
+        btn = QPushButton('티켓팅이동', self)
+        btn.move(80, 100)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(self.gotoTicketing)
+
+        #  버튼
+        btn = QPushButton('재시작', self)
+        btn.move(80, 170)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(self.reStart)
         
         # 로그인 버튼
         btn = QPushButton('로그인', self)
@@ -37,7 +54,7 @@ class MyApp(QWidget):
         btn = QPushButton('시작', self)
         btn.move(240, 170)
         btn.resize(btn.sizeHint())
-        btn.clicked.connect(self.startInterpark)
+        btn.clicked.connect(self.macroManager)
 
         # 종료 버튼
         btn2 = QPushButton('종료', self)
@@ -51,48 +68,134 @@ class MyApp(QWidget):
         # 창 띄우기
         self.show()
 
+    # 재시작
+    def reStart(self):
+        global driver
+        driver = webdriver.Chrome('C:\chromedriver.exe')
+        driver.set_window_size(800, 1000)  # (가로, 세로)
+
+        global step
+        step = 0
+
     # 인터파크 로그인
     def loginInterpark(self):
-        driver.set_window_size(800, 1000)  # (가로, 세로)
-        driver.get('https://ticket.interpark.com/Gate/TPLogin.asp') # 페이지 이동
+        try :
+            driver.get('https://ticket.interpark.com/Gate/TPLogin.asp') # 페이지 이동
 
-        driver.switch_to.frame(driver.find_element(By.XPATH, "//div[@class='leftLoginBox']/iframe[@title='login']"))
-        userId = driver.find_element(By.ID, 'userId')
-        userId.send_keys('won5854') # 로그인 할 계정 id
-        userPwd = driver.find_element(By.ID, 'userPwd')
-        userPwd.send_keys('skfkrh@816') # 로그인 할 계정의 패스워드
+            driver.switch_to.frame(driver.find_element(By.XPATH, "//div[@class='leftLoginBox']/iframe[@title='login']"))
+            userId = driver.find_element(By.ID, 'userId')
+            userId.send_keys('won5854') # 로그인 할 계정 id
+            userPwd = driver.find_element(By.ID, 'userPwd')
+            userPwd.send_keys('skfkrh@816') # 로그인 할 계정의 패스워드
+            time.sleep(5)
+            userPwd.send_keys(Keys.ENTER)
+            # driver.get('http://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=' + '22004761')
+            self.gotoTicketing()
+        
+        except:
+            traceback.print_exc()
 
-        # 문자열 인식
-        capchaPng = driver.find_element(By.XPATH, "//*[@id='oCaptchaFrame']") # 입력해야될 문자 이미지 캡쳐하기. iframe
-        reader = easyocr.Reader(['en']) # easyocr 이미지내 인식할 언어 지정
-        result = reader.readtext(capchaPng.screenshot_as_png, detail=0) # 캡쳐한 이미지에서 문자열 인식하기
+    def gotoTicketing(self):
+        driver.get('http://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=' + '22004761')
 
-        print(result[0])
+    # 매크로 매니저
+    def macroManager(self):
+        print("macroManager ::: " + str(step))
+        # 예매하기
+        if step == 0 :
+            self.startInterpark()
+        # 회차선택
+        elif step == 1 :
+            self.selDayHoicha()
+        # 좌석선택
+        elif step == 2 :
+            self.selSeat()
 
-        # 이미지에 점과 직선이 포함되어있어서 문자 인식 데이터 수동 보정
-        capchaValue = result[0].replace(' ', '').replace('$', 'S').replace(',', '')\
-            .replace(':', '').replace('.', '').replace('+', 'T').replace("'", '').replace('`', '')\
-            .replace('e', 'Q').replace('3', 'S').replace('€', 'C').replace('{', '').replace('-', '')
-
-        print(capchaValue)
-        chapchaText = driver.find_element(By.ID, 'oCheckCaptcha')
-        chapchaText.send_keys(capchaValue)
-        userPwd.send_keys(Keys.ENTER)
 
     # 인터파크 예매시작
     def startInterpark(self):
-        driver.get('http://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GoodsCode=' + '22004761')
-        # 예매하기 버튼 클릭
-        driver.find_element(By.XPATH, "//div[@class='sideBtnWrap']/a[@class='sideBtn is-primary']").click()
-        
-        # 예매하기 눌러서 새창이 뜨면 포커스를 새창으로 변경
-        driver.switch_to.window(driver.window_handles[1])
-        driver.get_window_position(driver.window_handles[1])
+        try :
+            # 예매하기 버튼 클릭
+            driver.find_element(By.XPATH, "//div[@class='sideBtnWrap']/a[@class='sideBtn is-primary']").click()
 
-        # 예매안내가 팝업이 뜨면 닫기. ( ticketingInfo_check : True, False )
-        ticketingInfo_check = self.check_exists_by_element(By.XPATH, "//div[@class='layerWrap']/div[@class='titleArea']/a[@class='closeBtn']")
-        if ticketingInfo_check:
-           driver.find_element(By.XPATH, "//div[@class='layerWrap']/div[@class='titleArea']/a[@class='closeBtn']").click()
+            global step
+            step = 1
+
+            # alert 확인버튼 누른 후 문자입력
+            # time.sleep(5)
+
+            # 예매안내가 팝업이 뜨면 닫기. ( ticketingInfo_check : True, False )
+            # ticketingInfo_check = self.check_exists_by_element(By.XPATH, "//div[@class='layerWrap']/div[@class='titleArea']/a[@class='closeBtn']")
+            # if ticketingInfo_check:
+            #    driver.find_element(By.XPATH, "//div[@class='layerWrap']/div[@class='titleArea']/a[@class='closeBtn']").click()
+        
+        except:
+            traceback.print_exc()
+
+    # 회차선택
+    def selDayHoicha(self):
+        print("selDayHoicha")
+
+        try :
+            # 테스트 회차 2022년 07월 15일 1회차
+            arr1 = [['20220715', 1]]
+            
+            # 예매하기 눌러서 새창이 뜨면 포커스를 새창으로 변경
+            driver.switch_to.window(driver.window_handles[1])
+            driver.get_window_position(driver.window_handles[1])
+
+            driver.switch_to.frame(driver.find_element(By.XPATH, "//div[@id='divBookSeat']/iframe[@id='ifrmSeat']"))
+
+            selDay = Select(driver.find_element(By.ID, 'PlayDate'))
+            selHoiCha = Select(driver.find_element(By.ID, 'PlaySeq'))
+
+            selDay.select_by_value(arr1[0][0])
+            time.sleep(0.5)
+            selHoiCha.select_by_index(arr1[0][1])
+            
+            global step
+            step = 2
+            self.macroManager()
+
+        except:
+            traceback.print_exc()
+
+    # 좌석선택
+    def selSeat(self):
+        print("selSeat")
+        
+        try :
+            # seats = driver.find_elements(By.XPATH, "//img[@class='stySeat' and contains(@title, 'A석') and contains(@title, '1층')]")
+            seats = driver.find_elements(By.XPATH, "//img[@class='stySeat']")
+            print(seats)
+            print(len(seats))
+
+            global step
+            step = 3
+            self.macroManager()
+
+        
+        except:
+            traceback.print_exc()
+
+
+    def easyocrChar(self):
+        print("easyocrChar")
+         # 문자열 인식
+        # capchaPng = driver.find_element(By.XPATH, "//*[@id='oCaptchaFrame']") # 입력해야될 문자 이미지 캡쳐하기. iframe
+        # reader = easyocr.Reader(['en']) # easyocr 이미지내 인식할 언어 지정
+        # result = reader.readtext(capchaPng.screenshot_as_png, detail=0) # 캡쳐한 이미지에서 문자열 인식하기
+
+        # print(result[0])
+
+        # # 이미지에 점과 직선이 포함되어있어서 문자 인식 데이터 수동 보정
+        # capchaValue = result[0].replace(' ', '').replace('$', 'S').replace(',', '')\
+        #     .replace(':', '').replace('.', '').replace('+', 'T').replace("'", '').replace('`', '')\
+        #     .replace('e', 'Q').replace('3', 'S').replace('€', 'C').replace('{', '').replace('-', '')
+
+        # print(capchaValue)
+        # chapchaText = driver.find_element(By.ID, 'oCheckCaptcha')
+        # chapchaText.send_keys(capchaValue)
 
     # 멜론티켓
     def startMelon(self):
