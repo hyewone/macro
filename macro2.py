88f4d204c8e9e8e07de4eeeb8923ee07c629d4f8
cheonhoonmin <cjsgnsals3@naver.com>
2022-06-25 17:06:54 +0900

    no message

--- macro2.py
+++ macro2.py
@@ -1,6 +1,6 @@
 import sys
 # GUI
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication
 
@@ -13,7 +13,6 @@ from selenium.webdriver.support.select import Select
 import easyocr
 import time
 import traceback
-import js2py
 
 driver = webdriver.Chrome('C:\chromedriver.exe')
 driver.set_window_size(800, 1000)  # (가로, 세로)
@@ -30,22 +29,53 @@ step = 0
 g_id = "won5854"
 # 패스워드 ex: skfkrh@816
 g_pwd = "skfkrh@816"
+# 생년월일
+g_birth = "951209"
 # 상품번호 ex: 22004761
 g_goodsNum = "22004761"
 # 날짜 ex: 20220715
 g_date = "20220715"
 # 회차 ex: 1, 2
-g_hoicha = 1
+g_hoicha = '1'
 # 층 ex: 1층
 g_floor = "1층"
 # 석 ex: S석, VIP석
 g_seatGrade = "S석"
 # 구역 ex: C열, E열
-g_block = ""
+g_block = "A열"
 
 # 테스트 회차 2022년 07월 15일 1회차
-arr1 = [[g_date, g_hoicha]]
+arr1 = [ ['20220715', 1]
+        ,['20220719', 1]
+        ,['20220720', 2]
+        ,['20220722', 2]
+        ,['20220724', 1]
+        ,['20220726', 1]
+        ,['20220727', 2]
+        ,['20220729', 1]
+        ,['20220731', 2]
+        ,['20220802', 1]
+        ,['20220803', 2]
+        ,['20220805', 2]
+        ,['20220807', 1]]
 
+g_date_arr = [   '20220715'
+                ,'20220719'
+                ,'20220720'
+                ,'20220722'
+                ,'20220724'
+                ,'20220726'
+                ,'20220727'
+                ,'20220729'
+                ,'20220731'
+                ,'20220802'
+                ,'20220803'
+                ,'20220805'
+                ,'20220807']
+g_hoicah_arr = ['1', '2']
+g_floor_arr = ['1층', '2층', '3층']
+g_grade_arr = ['VIP석', 'S석', 'A석', 'R석', 'B석']
+g_block_arr = ['A열', 'B열', 'C열', 'D열', 'E열']
 
 class MyApp(QWidget):
 
@@ -54,41 +84,154 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
+
         # 기본설정
-        self.setWindowTitle('My First Application')
-        self.move(1400, 300)
-        self.resize(400, 200)
-        # self.setGeometry(300, 300, 300, 200) : move + resize
+        self.setWindowTitle('Macro')
+        self.move(1200, 150)
+        self.resize(500, 700)
 
-        #  버튼
-        btn = QPushButton('티켓팅이동', self)
-        btn.move(80, 100)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(self.gotoTicketing)
+        # 설정저장
+        # btn = QPushButton('설정저장', self)
+        # btn.setGeometry(380, 10, 40, 25)
+        # btn.resize(btn.sizeHint())
+        # btn.clicked.connect(self.loginInterpark)
 
-        #  버튼
-        btn = QPushButton('재시작', self)
-        btn.move(80, 170)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(self.reStart)
+        # ID
+        labelId = QLabel('ID:', self)
+        labelId.setGeometry(30, 50, 70, 25)
+        self.lineEditId = QLineEdit(self)
+        self.lineEditId.setGeometry(100, 50, 110, 25)
+        self.lineEditId.textEdited.connect(self.changeInputId)
+
+        # PW
+        labelPw = QLabel('PW:', self)
+        labelPw.setGeometry(250, 50, 70, 25)
+        self.lineEditPw = QLineEdit(self)
+        self.lineEditPw.setGeometry(320, 50, 110, 25)
+        self.lineEditPw.textEdited.connect(self.changeInputPw)
+
+        # -----------------------------------------------
+
+        # 생년월일
+        labelBirth = QLabel('생년월일:', self)
+        labelBirth.setGeometry(30, 90, 70, 25)
+        self.lineEditBirth = QLineEdit(self)
+        self.lineEditBirth.setGeometry(100, 90, 110, 25)
+        self.lineEditBirth.textEdited.connect(self.changeInputBirth)
         
-        # 로그인 버튼
-        btn = QPushButton('로그인', self)
-        btn.move(160, 170)
-        btn.resize(btn.sizeHint())
+        label1 = QLabel('---------------------------------------------------', self)
+        label1.setGeometry(30, 125, 450, 25)
+
+        # -----------------------------------------------
+        
+        # 공연번호
+        labelNo = QLabel('공연번호:', self)
+        labelNo.setGeometry(30, 150, 70, 25)
+        self.lineEditNo = QLineEdit(self)
+        self.lineEditNo.setGeometry(100, 150, 110, 25)
+        self.lineEditNo.textEdited.connect(self.changeInputNo)
+
+        # 공연일자
+        labelDate = QLabel('공연일자:', self)
+        labelDate.setGeometry(250, 150, 70, 25)
+        self.cbDate = QComboBox(self)
+        self.cbDate.addItems(g_date_arr)
+        self.cbDate.setGeometry(320, 150, 110, 25)
+        self.cbDate.currentIndexChanged.connect(self.changeComboDate)
+
+        # -----------------------------------------------
+        
+        # 공연회차
+        labelHoicha = QLabel('공연회차:', self)
+        labelHoicha.setGeometry(250, 190, 70, 25)
+        self.cbHoicha = QComboBox(self)
+        self.cbHoicha.addItems(g_hoicah_arr)
+        self.cbHoicha.setGeometry(320, 190, 110, 25)
+        self.cbHoicha.currentIndexChanged.connect(self.changeComboHoicha)
+
+        label1 = QLabel('---------------------------------------------------', self)
+        label1.setGeometry(30, 225, 450, 25)
+
+        # -----------------------------------------------
+
+        # 좌석층
+        labelFoor = QLabel('좌석층:', self)
+        labelFoor.setGeometry(30, 250, 70, 25)
+        self.cbFoor = QComboBox(self)
+        self.cbFoor.addItems(g_floor_arr)
+        self.cbFoor.setGeometry(100, 250, 110, 25)
+        self.cbFoor.currentIndexChanged.connect(self.changeComboFloor)
+
+        # 좌석등급
+        labelGrade = QLabel('좌석등급:', self)
+        labelGrade.setGeometry(250, 250, 70, 25)
+        self.cbGrade = QComboBox(self)
+        self.cbGrade.addItems(g_grade_arr)
+        self.cbGrade.setGeometry(320, 250, 110, 25)
+        self.cbGrade.currentIndexChanged.connect(self.changeComboGrade)
+
+        # -----------------------------------------------
+
+        # 좌석구역
+        labelBlock = QLabel('구역:', self)
+        labelBlock.setGeometry(250, 290, 70, 25)
+        self.cbBlock = QComboBox(self)
+        self.cbBlock.addItems(g_block_arr)
+        self.cbBlock.setGeometry(320, 290, 110, 25)
+        self.cbBlock.currentIndexChanged.connect(self.changeComboBlock)
+
+        label1 = QLabel('---------------------------------------------------', self)
+        label1.setGeometry(30, 325, 450, 25)
+
+        # -----------------------------------------------
+
+        self.lineEditId.setText(g_id)
+        self.lineEditPw.setText(g_pwd)
+        self.lineEditBirth.setText(g_birth)
+        self.lineEditNo.setText(g_goodsNum)
+        self.cbDate.setCurrentText(g_date)
+        self.cbHoicha.setCurrentText(g_hoicha)
+        self.cbFoor.setCurrentText(g_floor)
+        self.cbGrade.setCurrentText(g_seatGrade)
+        self.cbBlock.setCurrentText(g_block)
+
+        # -----------------------------------------------
+
+        # 1. 로그인
+        btn = QPushButton('1. 로그인', self)
+        btn.setGeometry(30, 370, 200, 50)
         btn.clicked.connect(self.loginInterpark)
 
-         # 시작 버튼
-        btn = QPushButton('시작', self)
-        btn.move(240, 170)
-        btn.resize(btn.sizeHint())
+        # 2. 티켓팅 페이지 이동
+        btn = QPushButton('2. 티켓팅 이동', self)
+        btn.setGeometry(250, 370, 200, 50)
+        btn.clicked.connect(self.gotoTicketing)
+
+        # 3. 좌석선택
+        btn = QPushButton('3. 좌석 선택', self)
+        btn.setGeometry(30, 450, 200, 50)
         btn.clicked.connect(self.macroManager)
 
-        # 종료 버튼
-        btn2 = QPushButton('종료', self)
-        btn2.move(320, 170)
-        btn2.resize(btn2.sizeHint())
-        btn2.clicked.connect(QCoreApplication.instance().quit)
+        # 4. 좌석재선택
+        btn = QPushButton('4. 좌석 재선택', self)
+        btn.setGeometry(250, 450, 200, 50)
+        btn.clicked.connect(self.macroManager)
+
+        # 5. 회차재선택
+        btn = QPushButton('5. 회차 재선택', self)
+        btn.setGeometry(30, 530, 200, 50)
+        btn.clicked.connect(self.macroManager)
+
+        # 재시작
+        btn = QPushButton('6. 재시작', self)
+        btn.setGeometry(250, 530, 200, 50)
+        btn.clicked.connect(self.reStart)
+
+        # # 종료 버튼
+        # btn2 = QPushButton('종료', self)
+        # btn2.move(320, 170)
+        # btn2.resize(btn2.sizeHint())
+        # btn2.clicked.connect(QCoreApplication.instance().quit)
 
         # 아이콘
         self.setWindowIcon(QIcon('web.png')) 
@@ -268,6 +411,33 @@ class MyApp(QWidget):
         driver.set_window_size(1400, 1000)  # (가로, 세로)
         driver.get('https://ticket.interpark.com/Gate/TPLogin.asp') # 페이지 이동
 
+    def changeInputId(self):
+        global g_id
+        g_id = self.lineEditId.text()
+    def changeInputPw(self):
+        global g_pwd
+        g_pwd = self.lineEditPw.text()
+    def changeInputBirth(self):
+        global g_birth
+        g_birth = self.lineEditBirth.text()
+    def changeInputNo(self):
+        global g_goodsNum
+        g_goodsNum = self.lineEditNo.text()
+    def changeComboDate(self):
+        global g_date
+        g_date = self.cbDate.currentText()
+    def changeComboHoicha(self):
+        global g_hoicha
+        g_hoicha = self.cbHoicha.currentText()
+    def changeComboFloor(self):
+        global g_floor
+        g_floor = self.cbFoor.currentText()
+    def changeComboGrade(self):
+        global g_seatGrade
+        g_seatGrade = self.cbGrade.currentText()
+    def changeComboBlock(self):
+        global g_block
+        g_block = self.cbBlock.currentText()
 
 
 if __name__ == '__main__':
